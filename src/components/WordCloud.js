74155438daffffe13e747f9a08f98a132c81ecea
Anthony Preza <apreza.95@gmail.com>
2019-12-08 14:36:13 -0800

    wordcloud updates

--- src/components/WordCloud.js
+++ src/components/WordCloud.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Spinner } from 'reactstrap';
+import { Jumbotron, Spinner } from 'reactstrap';
 import ReactWordcloud from 'react-wordcloud';
 
 class WordCloud extends React.Component {
@@ -7,7 +7,7 @@ class WordCloud extends React.Component {
 		super(props);
 		this.state = {
 			loading: true,
-			wordCloud: null
+			wordcloud: null
 		};
 	}
 
@@ -15,23 +15,46 @@ class WordCloud extends React.Component {
 		const { getWordCloud } = this.props;
 		getWordCloud().then((res) => {
 			this.setState({
-				wordCloud: res.data,
+				wordcloud: res.data,
 				loading: false
 			});
 		});
 	}
 
 	render() {
-		const { loading, wordCloud } = this.state;
-		return loading ? (
-			<Spinner />
-		) : (
+		const { loading, wordcloud } = this.state;
+		const options = {
+			colors: [ '#fffa27' ],
+			enableTooltip: true,
+			deterministic: false,
+			fontFamily: 'impact',
+			fontSizes: [ 5, 60 ],
+			fontStyle: 'normal',
+			fontWeight: 'normal',
+			padding: 1,
+			rotations: 3,
+			rotationAngles: [ 0, 90 ],
+			scale: 'sqrt',
+			spiral: 'archimedean',
+			transitionDuration: 1000
+		};
+		return (
 			<Jumbotron className="text-white" style={{ backgroundColor: 'inherit' }}>
-				<h1> Your Wordcloud</h1>
-				<p className="lead">The words that appear most in your top tracks. Sound like you?</p>
-				<div style={{ width: '100%', height: '100%' }}>
-					<ReactWordcloud words={wordCloud} />
-				</div>
+				{loading ? (
+					<div>
+						<p className="lead">Casting a spell...</p>
+						<Spinner />
+					</div>
+				) : (
+					<div>
+						<h1> Your Wordcloud</h1>
+						<p className="lead">The words that appear most in your top tracks. Sound like you?</p>
+						<div style={{ height: '500px', width: '100%' }}>
+							{' '}
+							<ReactWordcloud words={wordcloud} options={options} />
+						</div>
+					</div>
+				)}
 			</Jumbotron>
 		);
 	}
